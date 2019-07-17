2ba0d9591ded00e2bb2d87806cc0e3af414ffb3a
Anthony <anthonyzhu132@gmail.com>
2019-07-17 22:25:34 +0000

    Added if statement to stop repition

---
+++ chorus-while.js
@@ -0,0 +1,11 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  if (repat === 5) {
+    console.log("Stop being so repetitve!");
+  }
+  console.log(chorus);
+  repeat++;
+}
+
+console.log("Until the sun comes up!");
