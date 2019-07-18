5ee1a13080ca2453a69fe1133d1ed2e715981b3d
Anthony <anthonyzhu132@gmail.com>
2019-07-18 08:21:57 +0000

    Defining functions within variables for hello.js and LoopyLighthouse defining what numbers are divisible by 4 and 3, and both

---
+++ hello.js
@@ -0,0 +1,7 @@
+const sayHello = function (name) {
+  console.log("Hello, " + name + " You need to jump off Knight Bridge");
+}
+
+sayHello("Anthony")
+sayHello("Anthony Zhu")
+sayHello("Anthony EJ Zhu")
